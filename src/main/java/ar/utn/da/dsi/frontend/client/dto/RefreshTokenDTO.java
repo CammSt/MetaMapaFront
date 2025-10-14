608b8691e87ef86ed0a17a3cbc5219f5a492a52a
Camila <56458193+CammSt@users.noreply.github.com>
2025-10-13 23:35:59 -0300

    intento de front

---
+++ src/main/java/ar/utn/da/dsi/frontend/client/dto/RefreshTokenDTO.java
@@ -0,0 +1,13 @@
+package ar.utn.da.dsi.frontend.client.dto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RefreshTokenDTO {
+	private String refreshToken;
+}
