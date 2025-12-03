eddd046724831504636ccf45be707cd7e2a4d0ed
FacundoPuig <fpuig@frba.utn.edu.ar>
2025-12-03 08:44:36 -0300

    ya funcionan las graficas

--- src/main/java/ar/utn/da/dsi/frontend/client/dto/output/SolicitudSpamDTO.java
+++ src/main/java/ar/utn/da/dsi/frontend/client/dto/output/SolicitudSpamDTO.java
@@ -1,6 +1,22 @@
 package ar.utn.da.dsi.frontend.client.dto.output;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data // <--- Genera Getters y Setters automáticamente
+@NoArgsConstructor
+@AllArgsConstructor
 public class SolicitudSpamDTO {
-  private Long cantidadSpam;
+
+  // El porcentaje de spam calculado
+  private Double porcentaje;
+
+  // CORRECCIÓN:
+  // El backend envía este dato como "cantidad" en el JSON.
+  // Nosotros queremos usarlo como "totalSolicitudes" en Java/HTML.
+  // Esta anotación hace la traducción automática.
+  @JsonProperty("cantidad")
   private Long totalSolicitudes;
 }
